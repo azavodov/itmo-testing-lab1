a11e2a8d3801428f70d28468e4ad542cd445dcec
Egor Dubenetskiy <egor@banka.space>
2019-09-22 19:56:46 +0300

    Implement locating Legs

--- src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/Legs.java
+++ src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/Legs.java
@@ -1,4 +1,18 @@
 package ru.ifmo.se.testing.edubenetskiy.lab1;
 
 public class Legs {
+    private final Person owner;
+    private Place place;
+
+    Legs(Person owner) {
+        this.owner = owner;
+    }
+
+    void setLocation(Place place) {
+        this.place = place;
+    }
+
+    public Place getLocation() {
+        return this.place;
+    }
 }
