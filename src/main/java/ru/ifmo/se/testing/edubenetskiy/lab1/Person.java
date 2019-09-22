e4f63bbda9a0593213db05edd847a61969c63a0c
Egor Dubenetskiy <egor@banka.space>
2019-09-22 19:55:43 +0300

    Continue implementing Person

--- src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/Person.java
+++ src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/Person.java
@@ -1,5 +1,8 @@
 package ru.ifmo.se.testing.edubenetskiy.lab1;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Person {
 
     private final String name;
@@ -11,16 +14,21 @@ public class Person {
     private Neck neck;
 
     private Place location;
+    private Person observedPerson;
 
     public Person(String name, Place location) {
+        this(name, location, new SingleHeadedNeck());
+    }
+
+    public Person(String name, Place location, Neck neck) {
         this.name = name;
 
         this.isNervous = false;
         this.isStunned = false;
         this.isBelievingEyes = true;
 
-        this.legs = new Legs();
-        this.neck = new Neck();
+        this.legs = new Legs(this);
+        this.neck = neck;
 
         this.location = location;
     }
@@ -38,22 +46,45 @@ public class Person {
     }
 
     public void lookAt(Person someone) {
-        throw new UnsupportedOperationException("not implemented yet"); // todo
+        log.info("{} looks at {}", this, someone);
+        this.observedPerson = someone;
+        if (someone.getNumberOfHeads() != this.getNumberOfHeads()) {
+            this.isStunned = true;
+        }
+    }
+
+    private int getNumberOfHeads() {
+        return neck.getNumberOfHeads();
     }
 
     public Place getLocation() {
-        throw new UnsupportedOperationException("not implemented yet"); // todo
+        return this.location;
     }
 
     public boolean isLocatedAt(Place place) {
-        throw new UnsupportedOperationException("not implemented yet"); // todo
+        return this.location.equals(place) ||
+               this.location.isInside(place);
     }
 
-    public boolean goTo(Place newLocation) {
-        throw new UnsupportedOperationException("not implemented yet"); // todo
+    public void goTo(Place newLocation) {
+        log.info("{} goes to {}", this, newLocation);
+        this.location = newLocation;
+        this.legs.setLocation(newLocation);
     }
 
-    public boolean follow(Person whom) {
-        throw new UnsupportedOperationException("not implemented yet"); // todo
+    public void follow(Person whom) {
+        log.info("{} follows {} into {}", this, whom, whom.getLocation());
+        this.isNervous = true;
+        this.goTo(whom.getLocation());
+    }
+
+    public void putLegsOnto(Place place) {
+        log.info("{} puts their legs onto {}", this, place);
+        legs.setLocation(place);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
     }
 }
