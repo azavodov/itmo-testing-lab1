038e4e4e8b947c41a4e6d384d52a9a73ae8617c5
Egor Dubenetskiy <egor@banka.space>
2019-10-09 00:32:44 +0300

    Add class Things

---
+++ src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/galaxy/Things.java
@@ -0,0 +1,13 @@
+package ru.ifmo.se.testing.edubenetskiy.lab1.galaxy;
+
+public class Things {
+    private long number;
+
+    public Things() {
+        number = 0;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+}
