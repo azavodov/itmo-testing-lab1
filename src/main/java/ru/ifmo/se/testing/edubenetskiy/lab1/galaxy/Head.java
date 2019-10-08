b20b55043d66625950b5834e9caae4efa3f3f209
Egor Dubenetskiy <egor@banka.space>
2019-10-09 00:33:52 +0300

    Add fields to Head

--- src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/galaxy/Head.java
+++ src/main/java/ru/ifmo/se/testing/edubenetskiy/lab1/galaxy/Head.java
@@ -1,4 +1,15 @@
 package ru.ifmo.se.testing.edubenetskiy.lab1.galaxy;
 
 public class Head {
+    private final Jaws jaws;
+    private final Neck ownerNeck;
+
+    Head(Neck ownerNeck) {
+        this.jaws = new Jaws();
+        this.ownerNeck = ownerNeck;
+    }
+
+    public Jaws getJaws() {
+        return jaws;
+    }
 }
