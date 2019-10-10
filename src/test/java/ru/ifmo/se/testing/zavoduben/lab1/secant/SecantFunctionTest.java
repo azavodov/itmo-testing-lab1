5e15feac7b8eb780e780a8124b8d4d960d8de69c
Egor Dubenetskii <egor.dubenetskii@sbb.ch>
2019-10-10 20:24:28 +0300

    Extend coverage of SecantFunction

--- src/test/java/ru/ifmo/se/testing/zavoduben/lab1/secant/SecantFunctionTest.java
+++ src/test/java/ru/ifmo/se/testing/zavoduben/lab1/secant/SecantFunctionTest.java
@@ -1,12 +1,12 @@
 package ru.ifmo.se.testing.zavoduben.lab1.secant;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.*;
 
-@Disabled
 class SecantFunctionTest {
 
     private SecantFunction testSubject;
@@ -17,24 +17,136 @@ class SecantFunctionTest {
     }
 
     @Test
+    void getValue_atNan_throwsException() {
+        assertThatCode(() -> testSubject.getValue(Double.NaN))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Secant is not defined for NaN");
+    }
+
+    @Test
+    void getValue_atMinusInf_throwsException() {
+        assertThatCode(() -> testSubject.getValue(Double.NEGATIVE_INFINITY))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Secant is not defined at infinity");
+    }
+
+    @Test
+    void getValue_atPlusInf_throwsException() {
+        assertThatCode(() -> testSubject.getValue(Double.POSITIVE_INFINITY))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Secant is not defined at infinity");
+    }
+
+    @Test
     void getValue_atZero_isPlusOne() {
         assertThat(testSubject.getValue(0.0D)).isCloseTo(+1.0D, within(0.1D));
     }
 
     @Test
-    void getValue_uptoPlusHalfPi_isPositive() {
+    void getValue_atPi_isMinusOne() {
+        assertThat(testSubject.getValue(Math.PI)).isCloseTo(-1.0D, within(0.1D));
+    }
+
+    @Test
+    void getValue_atMinusPi_isMinusOne() {
+        assertThat(testSubject.getValue(0 - Math.PI)).isCloseTo(-1.0D, within(0.1D));
+    }
+
+    @Test
+    void getValue_atTwoPiN_isPlusOne() {
+        double x = 2.0 * Math.PI * new Random().nextInt();
+        System.out.println("x = " + x);
+        assertThat(testSubject.getValue(x)).isCloseTo(+1.0D, within(0.1D));
+    }
+
+    @Test
+    void getValue_atPiPlusTwoPiN_isMinusOne() {
+        double x = Math.PI + new Random().nextInt() * 2 * Math.PI;
+        System.out.println("x = " + x);
+        int expected = -1;
+        double actual = testSubject.getValue(x);
+        assertThat(actual)
+                .isCloseTo(expected, withinPercentage(10L));
+    }
+
+    @Test
+    void getValue_atHalfPiPlusPiN_isIndefinite() {
+        double x = Math.PI / 2 + Math.PI * new Random().nextInt();
+        System.out.println("x = " + x);
+        double expected = Double.NaN;
+        double actual = testSubject.getValue(x);
+        assertThat(actual).isNaN();
+    }
+
+    @Test
+    void getValue_fromZeroUptoPlusHalfPi_isPositive() {
         double x = Math.random() * Math.PI / 2;
+        System.out.println("x = " + x);
         assertThat(testSubject.getValue(x))
                 .isCloseTo(1 / Math.cos(x), withinPercentage(10L));
     }
 
     @Test
-    void getValue_downtoMinusHalfPi_isPositive() {
+    void getValue_fromZeroDowntoMinusHalfPi_isPositive() {
         double x = -(Math.random() * Math.PI / 2);
+        System.out.println("x = " + x);
         double actualValue = testSubject.getValue(x);
         double expectedValue = 1 / Math.cos(x);
 
         assertThat(actualValue).isPositive();
+        assertThat(actualValue).isCloseTo(expectedValue, withinPercentage(10L));
+    }
+
+    @Test
+    void getValue_fromMinusHalfPiToZero_plusTwoPiN_isPositiveFalling() {
+        double offset = 2 * Math.PI * new Random().nextInt();
+        double x = -(Math.random() * Math.PI / 2) + offset;
+        System.out.println("x = " + x);
+
+        double actualValue = testSubject.getValue(x);
+        double expectedValue = 1 / Math.cos(x);
+
+        assertThat(actualValue).isPositive();
+        assertThat(actualValue).isCloseTo(expectedValue, withinPercentage(10L));
+    }
+
+    @Test
+    void getValue_fromZeroToHalfPi_plusTwoPiN_isPositiveRising() {
+        double offset = 2 * Math.PI * new Random().nextInt();
+        double x = (Math.random() * Math.PI / 2) + offset;
+        System.out.println("x = " + x);
+
+        double actualValue = testSubject.getValue(x);
+        double expectedValue = 1 / Math.cos(x);
+
+        assertThat(actualValue).isPositive();
+        assertThat(actualValue).isCloseTo(expectedValue, withinPercentage(10L));
+    }
+
+    @Test
+    void getValue_fromHalfPiToPi_plusTwoPiN_isNegativeRising() {
+        double offset = 2 * Math.PI * new Random().nextInt();
+        double x = (Math.random() * Math.PI / 2) + offset;
+        System.out.println("x = " + x);
+
+        double actualValue = testSubject.getValue(x);
+        double expectedValue = 1 / Math.cos(x);
+
+        assertThat(actualValue).isNegative();
+        assertThat(actualValue).isCloseTo(expectedValue, withinPercentage(10L));
+    }
+
+
+    @Test
+    void getValue_fromPiToThreeHalfsPi_plusTwoPiN_isNegativeFalling() {
+        double offset = 2 * Math.PI * new Random().nextInt();
+        double x = (Math.random() * Math.PI / 2) + offset;
+        System.out.println("x = " + x);
+
+        double actualValue = testSubject.getValue(x);
+        double expectedValue = 1 / Math.cos(x);
+
+        assertThat(actualValue).isNegative();
         assertThat(actualValue).isCloseTo(expectedValue, withinPercentage(10L));
     }
 }
