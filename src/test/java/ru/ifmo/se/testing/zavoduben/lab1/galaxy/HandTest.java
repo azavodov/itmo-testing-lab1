f614b000eadfcdc0767be20c502c4f53b4bb83dc
Egor Dubenetskiy <egor@banka.space>
2019-10-10 05:18:41 +0300

    Add HandTest

---
+++ src/test/java/ru/ifmo/se/testing/zavoduben/lab1/galaxy/HandTest.java
@@ -0,0 +1,28 @@
+package ru.ifmo.se.testing.zavoduben.lab1.galaxy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.ifmo.se.testing.zavoduben.lab1.galaxy.JawsAssert.assertThat;
+
+class HandTest {
+
+    private Hand testSubject;
+    private Person person;
+
+    @BeforeEach
+    void setUp() {
+        this.person = new PersonStubs(new PlaceStubs()).withOneHead();
+        this.testSubject = person.getLeftHand();
+    }
+
+    @Test
+    void pickTeeth_makesTeethClean() {
+        Jaws jaws = ((SingleHeadedNeck) person.getNeck()).getHead().getJaws();
+        assertThat(jaws).doesNotHaveCleanTeeth();
+
+        testSubject.pickTeeth(jaws);
+        assertThat(jaws).hasCleanTeeth();
+    }
+}
